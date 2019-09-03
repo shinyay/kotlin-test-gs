fd183721a3f33677d4a92e6b1dea43ecba4cf564
shinyay <shinya.com@gmail.com>
2019-09-03 21:23:41 +0900

    Add simple test case

---
+++ src/test/kotlin/io/pivotal/shinyay/test/HelloWorldTest.kt
@@ -0,0 +1,26 @@
+package io.pivotal.shinyay.test
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+@Tag("unitTest")
+class HelloWorldTest {
+
+    lateinit var service: HelloWorld
+
+    @BeforeEach
+    fun configureSystemUnderTest() {
+        service = HelloWorld()
+    }
+
+    @Test
+    @DisplayName("Should return the correct message")
+    fun shouldReturnCorrectMessage() {
+
+        println(service.getGreeting())
+        assertThat(service.getGreeting()).isEqualTo("Hello!")
+    }
+}
