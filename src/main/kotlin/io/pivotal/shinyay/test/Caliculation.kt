863997a67032135fd3e1524ff691d743622dc8b0
shinyay <shinya.com@gmail.com>
2019-09-04 00:24:50 +0900

    Add test target logic

---
+++ src/main/kotlin/io/pivotal/shinyay/test/Caliculation.kt
@@ -0,0 +1,8 @@
+package io.pivotal.shinyay.test
+
+class Caliculation {
+
+    fun sum(param1: Int, param2: Int) = param1 + param2
+
+    fun div(param1: Int, param2: Int):Double = (param1 / param2).toDouble()
+}
